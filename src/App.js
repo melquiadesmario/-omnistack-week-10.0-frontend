c5b34c9fc336dabbb34d9b43d55aa407c209cca1
melquiadesmario <melquiadesm@gmail.com>
2020-04-21 23:15:12 -0300

    Main Style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import './global.css'
 import './App.css'
 import './Sidebar.css'
+import './Main.css'
 
 const App = () => {
   return(
@@ -11,7 +12,7 @@ const App = () => {
         <strong>Register</strong>
         <form>
           <div className='input-block'>
-            <label htmlFor='github_username'>User GitHub</label>
+            <label htmlFor='github_username'>User Github</label>
             <input
               name='github_username'
               id='github_username'
@@ -52,7 +53,52 @@ const App = () => {
         </form>
       </aside>
       <main>
-
+        <ul>
+          <li className='dev-item'>
+            <header>
+              <img src='https://avatars2.githubusercontent.com/u/24658474?s=460&u=3431e2a69457286717db18bbb6083ea3956337fa&v=4' alt='Melquíades Mário' />
+              <div className='user-info'>
+                <strong>Melquíades Mário</strong>
+                <span>ReactJS, React Native, Node.js, MongoDB</span>
+              </div>
+            </header>
+            <p>Developer and enthusiast of javascript technologies, such as Node.js, React JS, React Native and MongoDB.</p>
+            <a href='https://github.com/melquiadesmario'>Access Github Profile</a>
+          </li>
+          <li className='dev-item'>
+            <header>
+              <img src='https://avatars2.githubusercontent.com/u/24658474?s=460&u=3431e2a69457286717db18bbb6083ea3956337fa&v=4' alt='Melquíades Mário' />
+              <div className='user-info'>
+                <strong>Melquíades Mário</strong>
+                <span>ReactJS, React Native, Node.js, MongoDB</span>
+              </div>
+            </header>
+            <p>Developer and enthusiast of javascript technologies, such as Node.js, React JS, React Native and MongoDB.</p>
+            <a href='https://github.com/melquiadesmario'>Access Github Profile</a>
+          </li>
+          <li className='dev-item'>
+            <header>
+              <img src='https://avatars2.githubusercontent.com/u/24658474?s=460&u=3431e2a69457286717db18bbb6083ea3956337fa&v=4' alt='Melquíades Mário' />
+              <div className='user-info'>
+                <strong>Melquíades Mário</strong>
+                <span>ReactJS, React Native, Node.js, MongoDB</span>
+              </div>
+            </header>
+            <p>Developer and enthusiast of javascript technologies, such as Node.js, React JS, React Native and MongoDB.</p>
+            <a href='https://github.com/melquiadesmario'>Access Github Profile</a>
+          </li>
+          <li className='dev-item'>
+            <header>
+              <img src='https://avatars2.githubusercontent.com/u/24658474?s=460&u=3431e2a69457286717db18bbb6083ea3956337fa&v=4' alt='Melquíades Mário' />
+              <div className='user-info'>
+                <strong>Melquíades Mário</strong>
+                <span>ReactJS, React Native, Node.js, MongoDB</span>
+              </div>
+            </header>
+            <p>Developer and enthusiast of javascript technologies, such as Node.js, React JS, React Native and MongoDB.</p>
+            <a href='https://github.com/melquiadesmario'>Access Github Profile</a>
+          </li>
+        </ul>
       </main>
     </div>
   )
